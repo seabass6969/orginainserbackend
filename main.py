e3d15b73fe01d81dd0075193b4cc26348b432216
BuildTools <cephas8080@outlook.com>
2022-04-06 13:39:47 +0100

    test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
  
 app = Flask(__name__)
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
         return "<h1>first</h1>"
+@app.route("/test")
+def test():
+    return os.environ.get('TEST')
